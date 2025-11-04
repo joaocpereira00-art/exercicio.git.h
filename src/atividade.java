ffb54492e661a0207e57721f231e141caa81a234
joao carlos <joaocpereira00@gmail.com>
2025-11-03 22:00:33 -0300

    Pedido de comida

---
+++ src/atividade.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+ import java.util.Scanner;
+
+public class atividade {
+ public static void main(String[] args) throws Exception {
+    
+    Scanner ler = new Scanner(System.in);
+
+    int opcao;
+
+    System.out.println("Selecione uma opcao abaixo no menu");
+    System.out.println("1 X-tudo(R$ 15,00) ");
+    System.out.println("2- X-Becon(R$ 18,00)");
+    System.out.println("3- Refrigerante (R$ 6,00) ");
+    System.out.println("4- Suco (R$ 8,00)");
+    opcao = ler.nextInt();
+
+    switch (opcao) {
+        case 1:
+            System.out.println("Pedido computado !");
+            System.out.println("X-tudo (R$ 15,00)");
+            break;
+        case 2:
+            System.out.println("Pedido computado !");
+            System.out.println("X-tudo (R$ 15,00)");
+            break;
+        case 3:
+             System.out.println("Pedido computado !");
+             System.out.println("X-Becon (R$ 18,00) ");
+             break;
+        case 4:
+             System.out.println("Pedido computado !");
+             System.out.println("Refrigerante (R$ 6,00) ");
+             break;
+        case 5:
+             System.out.println("Pedido computado !");
+             System.out.println("Suco (R$ 8,00) ");
+             break;
+        default:
+             System.out.println("Opcao invalida! ");
+             break;
+
+    }
+}
+}
