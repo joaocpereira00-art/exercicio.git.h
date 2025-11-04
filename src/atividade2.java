c0739a903894e7fcde330193315748a8002f53d4
joao carlos <joaocpereira00@gamail.com>
2025-11-03 22:51:38 -0300

    nota

---
+++ src/atividade2.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class atividade2 {
+
+    public static void main(String[] args) throws Exception {
+    
+        Scanner ler = new Scanner(System.in);
+
+        int i;
+
+        for(i=1; i<=4; i++){
+            System.out.println("Digite a "+i+" nota:");
+        }
+}
+}
